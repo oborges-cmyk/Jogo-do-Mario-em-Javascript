e18d57d4a9bd57c6ee7818eae787ce9dae61c2d5
otavio <o.borges@escola.pr.gov.br>
2026-09-08 10:30:06 -0300

    Update game.js

--- game.js
+++ game.js
@@ -899,3 +899,73 @@ function drawGround(cx) {
     
       ctx.restore();
     }
+
+    function drawMario(cx) {
+      const sx = mario.x - cx;
+      const sy = mario.y;
+      const dir = mario.dir;
+    
+      if (mario.dead) {
+        // Dead Mario (flat spin)
+        ctx.save();
+        ctx.translate(sx + mario.w / 2, sy + mario.h / 2);
+        ctx.rotate(mario.deadTimer * 0.15);
+        drawMarioSprite(0, 0, 1, 0);
+        ctx.restore();
+        return;
+      }
+    
+      if (mario.invincible > 0 && Math.floor(mario.invincible / 3) % 2 === 0) return;
+    
+      ctx.save();
+      ctx.translate(sx + mario.w / 2, sy + mario.h);
+      ctx.scale(dir, 1);
+      drawMarioSprite(0, 0, mario.frame, mario.onGround ? 0 : 1);
+      ctx.restore();
+    }
+
+    function drawMarioSprite(ox, oy, frame, airborne) {
+      // ...
+      ctx.fillStyle = C.mario.hat;
+      ctx.fillRect(ox - 10, oy - 32, 20, 6);
+      ctx.fillRect(ox - 7, oy - 38, 16, 8);
+  
+      // Face / skin (Rosto / pele)
+      ctx.fillStyle = C.mario.skin;
+      ctx.fillRect(ox - 8, oy - 26, 16, 10);
+
+      // Eyes (Olhos)
+      ctx.fillStyle = '#000';
+      ctx.fillRect(ox + 1, oy - 24, 4, 3);
+
+// Mustache (Bigode)
+      ctx.fillStyle = C.mario.shoes;
+ctx.fillRect(ox - 8, oy - 18, 6, 3);
+ctx.fillRect(ox,     oy - 18, 6, 3);
+
+// Overalls body (Corpo do macacão)
+ctx.fillStyle = C.mario.overalls;
+ctx.fillRect(ox - 10, oy - 18, 20, 14);
+
+// ... [linhas borradas acima, parecem desenhar botões]
+946     ctx.fillStyle = C.mario.skin;
+947     ctx.fillRect(ox - 6, oy - 22, 4, 4);
+948     ctx.fillRect(ox + 2, oy - 22, 4, 4);
+949 
+950     // Legs / feet
+951     const legOff = airborne ? [0, 0] : frame === 1 ? [-4, 4] : frame === 2 ? [4, -4] :;
+952     ctx.fillStyle = C.mario.overalls;
+953     ctx.fillRect(ox - 10, oy - 4 + legOff[0], 8, 8);
+954     ctx.fillRect(ox + 2, oy - 4 + legOff[1], 8, 8);
+955 
+956     // Shoes
+957     ctx.fillStyle = C.mario.shoes;
+958     ctx.fillRect(ox - 12, oy + 2 + legOff[0], 12, 6);
+959     ctx.fillRect(ox + 1, oy + 2 + legOff[1], 12, 6);
+960 
+961     // Arms
+962     ctx.fillStyle = C.mario.skin;
+963     const armY = airborne ? -22 : -16;
+964     ctx.fillRect(ox - 16, oy + armY, 6, 10);
+965     ctx.fillRect(ox + 10, oy + armY, 6, 10);
+966 }
